c846e8e38a81aaffca2bce8cea6bc434f8f4d679
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-11 17:58:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ function BoardMember(name, homeState, training) {
 }
 
 BoardMember.prototype.veto = function() { 
-  console.log('No, I must disagree') 
+  console.log('No, I must disagree'); 
 } 
 
 BoardMember.prototype.approve = function() {
-  console.log('You can do that!')
+  console.log('You can do that!');
 } 
 
 BoardMember.prototype.doCharity = function() { 
-  console.log('I lie to help people')
+  console.log('I lie to help people');
 } 
 
 BoardMember.prototype.releasePressStatement = function() { 
-  console.log('You will see greate things from Scuber') 
+  console.log('You will see greate things from Scuber');
 } 
 
 BoardMember.prototype.sayHi = function() { 
-  console.log(`Hi my name is ${this.name}. I am from ${this.homeState} and I was trained in ${this.training}.`)
+  console.log(`Hi my name is ${this.name}. I am from ${this.homeState} and I was trained in ${this.training}.`);
 }
