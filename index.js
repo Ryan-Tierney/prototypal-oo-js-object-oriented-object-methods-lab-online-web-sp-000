685e7fe5b29b8af6841ec920762f4c7137d407d4
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-11 18:02:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-function BoardMember(name, homeState, training) { 
+function boardMember(name, homeState, training) { 
   this.name = name; 
   this.homeState = homeState; 
   this.training = training; 
 }
 
-BoardMember.prototype.veto = function() { 
+boardMember.prototype.veto = function() { 
   console.log('No, I must disagree'); 
 } 
 
-BoardMember.prototype.approve = function() {
+boardMember.prototype.approve = function() {
   console.log('You can do that!');
 } 
 
-BoardMember.prototype.doCharity = function() { 
+boardMember.prototype.doCharity = function() { 
   console.log('I lie to help people');
 } 
 
-BoardMember.prototype.releasePressStatement = function() { 
+boardMember.prototype.releasePressStatement = function() { 
   console.log('You will see greate things from Scuber');
 } 
 
-BoardMember.prototype.sayHi = function() { 
+boardMember.prototype.sayHi = function() { 
   console.log(`Hi my name is ${this.name}. I am from ${this.homeState} and I was trained in ${this.training}.`);
 }
